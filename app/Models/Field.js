1d140a588197da29d39cf0ca52f36cca2955df8b
Quentin Machard <quentin.machard@gmail.com>
2016-04-07 11:25:45 +0200

    Create Field

---
+++ app/Models/Field.js
@@ -0,0 +1,34 @@
+import EventEmitter from '../Core/EventEmitter.js';
+import Tank from './Tank.js';
+
+export default class Field extends EventEmitter {
+	constructor() {
+		super();
+
+		this.config = {
+			harvestMaturity: 10,
+			harvestIncrement: 1,
+			waterDecrement: 1
+		};
+
+		this.harvestLevel = 0;
+		this.tank = new Tank(3, 3);
+	}
+
+	grow() {
+		if(this.tank.getWater(this.config.waterDecrement) && this.harvestLevel < this.config.harvestMaturity) {
+			this.harvestLevel = Math.roundDecimal(this.harvestLevel + this.config.harvestIncrement, 2);
+
+			this.emit('grow');
+			this.emit('update');
+
+			this.checkMaturity();
+		}
+	}
+
+	checkMaturity() {
+		if(this.harvestLevel == this.config.harvestMaturity) {
+			this.emit('isMature')
+		}
+	}
+}
