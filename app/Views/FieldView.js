679e63df6bfb81b985501e25b0b214d13b85467a
Quentin Machard <quentin.machard@gmail.com>
2016-04-07 14:07:40 +0200

    Binding button on the Field View

--- app/Views/FieldView.js
+++ app/Views/FieldView.js
@@ -1,11 +1,18 @@
-export default class FieldView {
+import EventEmitter from '../Core/EventEmitter.js';
+
+export default class FieldView extends EventEmitter {
 	constructor(field, id) {
+		super();
+
 		this.field = field;
 
 		this.fieldDiv = document.getElementById(id);
 
 		this.harvestDiv = this.fieldDiv.querySelector('.harvest');
 		this.tankDiv = this.fieldDiv.querySelector('.quantity');
+
+		this.harvestButton = this.fieldDiv.querySelector('.btn-harvest');
+		this.irrigateButton = this.fieldDiv.querySelector('.btn-irrigate');
 
 		this.init();
 		this.initEvents();
@@ -25,6 +32,22 @@ export default class FieldView {
 		this.field.tank.on('update', () => {
 			this.updateTank();
 		});
+
+		this.field.on('isMature', () => {
+			this.harvestButton.removeAttribute('disabled');
+		});
+
+		this.field.on('reset', () => {
+			this.harvestButton.setAttribute('disabled', true);
+		});
+
+		this.harvestButton.addEventListener('click', () => {
+			this.emit('harvest');
+		}, false);
+
+		this.irrigateButton.addEventListener('click', () => {
+			this.emit('irrigate');
+		}, false);
 	}
 
 	updateHarvest() {
